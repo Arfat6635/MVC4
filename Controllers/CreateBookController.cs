ab0723c8e78386b79e933e3f4a1246c45377b3b3
Arfat <arfat3060@gmail.com>
2024-04-15 15:47:49 +0530

    Entity Add book demo

--- Controllers/CreateBookController.cs
+++ Controllers/CreateBookController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class CreateBookController : Controller
     {
-        
+        ApplicationDbContext context = new ApplicationDbContext();
         // GET: CreateBook
         public ActionResult Index()
         {
@@ -19,6 +20,20 @@ namespace WebApp.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        public ActionResult Save(AddBook book)
+        {
+            if (ModelState.IsValid) {
+                context.AddBooks.Add(book);
+              int result =   context.SaveChanges();
+
+                string Message = result == 0 ? "Insertion Failed" : "Inserted Successfully!";
+                TempData["Message"]=Message;
+
+                
+            }
+            return RedirectToAction("Create");
         }
     }
 }
